fb31400421c82dd1f6214d12d9c683f2c6fd04f9
Akashdeep1998 <43042131+Akashdeep1998@users.noreply.github.com>
2018-10-18 12:33:03 -0400

    done

--- week-5-lab.cpp
+++ week-5-lab.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include <fstream>
 using namespace std;
 int main ()
 {
@@ -12,7 +13,10 @@ cin >> b;
 auto c=sqrt (pow(a,2)+pow(b,2));
 
 cout <<"The third side ="<<c<<endl;
-
+std::ofstream myfile;
+myfile.open ("example.txt");
+if (myfile << "Your triangle has three sides that are "<<a<<" "<<b<<" and"<<c);
+myfile.close();
 return 0;
 
 }
